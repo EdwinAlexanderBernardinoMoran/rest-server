4a9335194be50717b3ad3d58115c7c341d754b20
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-03-20 06:38:18 -0600

    Creating the server with express and its middlewares

--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
-const hello = 'Hello World';
-console.log(hello);
+import dotenv from 'dotenv';
+import Server from './models/server';
+dotenv.config();
+
+const server = new Server();
+server.listen();
